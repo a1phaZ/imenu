44511371d827b5ba9d4747c7850f50c651428c68
a1phaZ <pride.ots@gmail.com>
2017-02-08 10:19:48 +0500

    initialize models

--- models/Category.js
+++ models/Category.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-	title: {type: String},
+	title      : {type: String},
 	description: {type: String},
-	items: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: ' Dish'
+	items      : {
+		type     : mongoose.Schema.Types.ObjectId,
+		ref      : 'Dish'
 	}
 });
 
---
+++ models/Dish.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const dishSchema = new mongoose.Schema({
+	title      : {type: String},
+	description: {type: String},
+	composition: Array,
+	category   : {
+		type     : mongoose.Schema.Types.ObjectId,
+		ref      : 'Category'
+	},
+	price      : {type: Number},
+	waiting    : {type: Number},
+	discount   : {type: Number}
+});
+
+const Dish = mongoose.model('Dish', dishSchema);
+
+module.exports = Dish;
