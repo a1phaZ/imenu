98f1016495bb43a1007366b3101ca7c7ed5b7c30
alphaZ_ <pride.ots@gmail.com>
2017-02-09 21:19:43 +0500

    test constants

---
+++ config/constants.js
@@ -0,0 +1,9 @@
+exports = {
+	getCategoryList : '/category',
+	getNewCategory : '/category/new',
+	postNewCategory : '/category/new',
+	getCategoryBySlug : '/category/:slug',
+	postCategoryBySlug : '/category/:slug',
+	getDishesList : '/dishes',
+	getDishesListByCategorySlug: '/category/:slug/dishes'
+}
