60019ccb55a9a416693780ab3e322c7688f34b6f
a1phaZ <pride.ots@gmail.com>
2017-02-13 09:18:51 +0500

    user

--- config/constants.js
+++
@@ -1,9 +0,0 @@
-exports = {
-	getCategoryList : '/category',
-	getNewCategory : '/category/new',
-	postNewCategory : '/category/new',
-	getCategoryBySlug : '/category/:slug',
-	postCategoryBySlug : '/category/:slug',
-	getDishesList : '/dishes',
-	getDishesListByCategorySlug: '/category/:slug/dishes'
-}
