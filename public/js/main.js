40e800ad0fb883646c49f8768d78000398691bdf
a1phaZ <pride.ots@gmail.com>
2017-02-21 10:52:26 +0500

    add cart local handler

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
 
   // Place JavaScript code here...
+  //add to cart handler
+  function addToCart(e) {
+    e.preventDefault();
+    if (e.target.attributes['data-product-id']){
+      var cartItem = e.target.attributes['data-product-id'].value;
+      var products = localStorage.products ? JSON.parse(localStorage.products) : [];
+      products.push(cartItem);
+      localStorage.products = JSON.stringify(products);
+    }
+  }
+
+  //add handler add to cart
+  var elements = $('.cart-add-btn');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', addToCart);
+  }
+  
+  //Breadcrumbs
   function getBreadcrumbs() {
   	var location = window.location;
   	var origin = location.origin;
   	var path = location.pathname;
   	var crumbs = (path != '/') ? path.split('/') : [];
-  	console.log(location);
-  	console.log(crumbs);
   	var breadcrumb = $('#breadcrumb');
   	if (crumbs.length != 0){
   		var url = origin;
